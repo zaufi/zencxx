a912043ead4712a1caf2d95e98e87865a2436e00
zaufi <i.zaufi@gmail.com>
2013-05-20 05:32:35 +0400

    add header w/ ticker forward declaration

---
+++ zencxx/ticker_fwd.hh
@@ -0,0 +1,40 @@
+/**
+ * \file
+ *
+ * \brief Forward declaration for \c zencxx::ticker class
+ *
+ * \date Mon May 20 05:30:33 MSK 2013 -- Initial design
+ */
+/*
+ * Copyright (C) 2010-2013 Alex Turbov and contributors, all rights reserved.
+ * This is free software. It is licensed for use, modification and
+ * redistribution under the terms of the GNU Lesser General Public License,
+ * version 3 or later <http://gnu.org/licenses/lgpl.html>
+ *
+ * ZenCxx is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ZenCxx is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.";
+ */
+
+#ifndef __ZENCXX__TICKER_FWD_HH__
+# define __ZENCXX__TICKER_FWD_HH__
+
+// Project specific includes
+
+// Standard includes
+
+namespace zencxx {
+
+class ticker;
+
+}                                                           // namespace zencxx
+#endif                                                      // __ZENCXX__TICKER_FWD_HH__
