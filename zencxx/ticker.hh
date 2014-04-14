6d3f4c9067c28197f3ccf543b92a15a98bfdf01c
Alex Turbov <i.zaufi@gmail.com>
2014-04-14 12:56:32 +0400

    msvc do not support noexecpt

--- zencxx/ticker.hh
+++ zencxx/ticker.hh
@@ -32,6 +32,7 @@
 
 // Standard includes
 #include <boost/asio/io_service.hpp>
+#include <boost/config.hpp>
 #include <boost/thread/lock_types.hpp>
 #include <boost/thread/mutex.hpp>
 #include <cassert>
@@ -85,8 +86,8 @@ public:
         job() : m_job_id(0) {}
         job(const job&) = default;                          ///< Default copy ctor
         job& operator=(const job&) = default;               ///< Default copy-assign operator
-        job(job&&) noexcept;                                ///< Move ctor
-        job& operator=(job&&) noexcept;                     ///< Move-assign operator
+        job(job&&) BOOST_NOEXCEPT;                          ///< Move ctor
+        job& operator=(job&&) BOOST_NOEXCEPT;               ///< Move-assign operator
 
         enum struct start_type : unsigned
         {
@@ -204,13 +205,13 @@ inline ticker::job::job(
 {
 }
 
-inline ticker::job::job(job&& other) noexcept
+inline ticker::job::job(job&& other) BOOST_NOEXCEPT
   : m_ticker_wptr(std::move(other.m_ticker_wptr))
   , m_job_id(other.m_job_id)
 {
 }
 
-inline ticker::job& ticker::job::operator=(job&& other) noexcept
+inline ticker::job& ticker::job::operator=(job&& other) BOOST_NOEXCEPT
 {
     if (this != &other)
     {
