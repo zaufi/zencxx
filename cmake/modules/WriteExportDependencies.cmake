ccd0a8d48000b1819ef63c024f50e52b8b2653c4
Alex Turbov <i.zaufi@gmail.com>
2014-11-01 23:34:46 +0300

    update chewy modules

--- cmake/modules/WriteExportDependencies.cmake
+++ cmake/modules/WriteExportDependencies.cmake
@@ -1,5 +1,7 @@
 # - Write an export dependencies file
 #
+# TODO More elaborate docs
+#
 
 #=============================================================================
 # Copyright 2014 by Alex Turbov <i.zaufi@gmail.com>
@@ -19,24 +21,48 @@ include(CMakeParseArguments)
 set(_WED_BASE_DIR "${CMAKE_CURRENT_LIST_DIR}")
 
 function(write_export_dependencies)
-    set(options)
-    set(one_value_args TARGET)
+    set(options APPEND)
+    set(one_value_args TARGET FILE_PREFIX DEPENDED_FILE_PREFIX)
     set(multi_value_args DEPENDENCIES)
     cmake_parse_arguments(_WED "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
+
+    if(NOT _WED_FILE_PREFIX)
+        set(_WED_FILE_PREFIX "${CMAKE_PROJECT_NAME}")
+    endif()
+
+    if(NOT _WED_DEPENDED_FILE_PREFIX)
+        set(_WED_DEPENDED_FILE_PREFIX "${_WED_FILE_PREFIX}")
+    endif()
 
     # Check if TARGET given
     if(_WED_TARGET)
         get_target_property(_wed_export_filename_part ${_WED_TARGET} EXPORT_NAME)
+    else()
+        message(FATAL_ERROR "TARGET parameter is mandatory when call write_export_dependencies()")
     endif()
 
     # Produce dependencies file only if there some dependencies provided
+    set(_wed_input_file "${_WED_BASE_DIR}/export-dependencies.cmake.in")
+    set(
+        _wed_output_file
+        "${CMAKE_CURRENT_BINARY_DIR}/${_WED_FILE_PREFIX}-${_wed_export_filename_part}-dependencies.cmake"
+      )
     if(_WED_DEPENDENCIES)
-        configure_file(
-            "${_WED_BASE_DIR}/export-dependencies.cmake.in"
-            "${CMAKE_CURRENT_BINARY_DIR}/zencxx-${_wed_export_filename_part}-dependencies.cmake"
-            @ONLY
-          )
+        if(NOT _WED_APPEND OR (NOT EXISTS "${_wed_input_file}" AND _WED_APPEND))
+            configure_file("${_wed_input_file}" "${_wed_output_file}" @ONLY)
+        else()
+            configure_file("${_wed_input_file}" "${_wed_output_file}.tmp" @ONLY)
+            file(READ "${_wed_output_file}.tmp" _wed_generated_content)
+            file(APPEND "${_wed_output_file}" "${_wed_generated_content}")
+            file(REMOVE "${_wed_output_file}.tmp")
+        endif()
     else()
         message(FATAL_ERROR "DEPENDENCIES parameter is mandatory when call write_export_dependencies()")
     endif()
 endfunction()
+
+# X-Chewy-RepoBase: https://raw.githubusercontent.com/mutanabbi/chewy-cmake-rep/master/
+# X-Chewy-Path: WriteExportDependencies.cmake
+# X-Chewy-Version: 1.3
+# X-Chewy-Description: Write an export dependencies file
+# X-Chewy-AddonFile: export-dependencies.cmake.in
